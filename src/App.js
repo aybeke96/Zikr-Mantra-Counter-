a57bd7cee1339be09fd0a0c045d3f86dc25a5373
aybeke96 <118341896+aybeke96@users.noreply.github.com>
2023-04-25 16:41:44 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      count:0
+    }
+  }
+  increment=()=>{
+    this.setState({count:this.state.count+1});
+  }
+  decrement=()=>{
+    this.setState({count:this.state.count-1});
+  }
+  reset=()=>{
+    this.setState({count:this.state.count=0});
+  }
+  render() {
+    return (<>
+    <div className="App">
+      <button onClick={this.decrement} className="counter">-</button>
+      <h2>{this.state.count}</h2>
+      <button onClick={this.increment} className="counter">+</button>
+    </div>
+    <div className="App">
+      <button onClick={this.reset} className="reset">reset</button>
+    </div>
+    </>);
+  }
+}
+
+export default App;
